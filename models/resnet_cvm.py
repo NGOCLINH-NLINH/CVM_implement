73e94ef4e2d649e0a515519451ef2e4685c1b0ec
NGOCLINH-NLINH <124993579+NGOCLINH-NLINH@users.noreply.github.com>
2026-05-11 00:14:53 +0700

    thu lai sparse

--- models/resnet_cvm.py
+++ models/resnet_cvm.py
@@ -23,15 +23,15 @@ class ResNetCVM(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
 
-        # if self.sparsity_ratio < 1.0:
-        #     k = max(1, int(x.size(1) * self.sparsity_ratio))
-        #     # _, idx = torch.topk(x, k, dim=1)
-        #     # mask = torch.full_like(x, self.leak)
-        #     # mask.scatter_(1, idx, 1.0)
-        #     # x = x * mask
-        #     val, idx = torch.topk(x, k, dim=1)
-        #     sparse_x = torch.zeros_like(x).scatter_(1, idx, val)
-        #     x = sparse_x
+        if self.sparsity_ratio < 1.0:
+            k = max(1, int(x.size(1) * self.sparsity_ratio))
+            # _, idx = torch.topk(x, k, dim=1)
+            # mask = torch.full_like(x, self.leak)
+            # mask.scatter_(1, idx, 1.0)
+            # x = x * mask
+            val, idx = torch.topk(x, k, dim=1)
+            sparse_x = torch.zeros_like(x).scatter_(1, idx, val)
+            x = sparse_x
 
         x = self.fc(x)
         x = F.normalize(x, p=2, dim=1)
