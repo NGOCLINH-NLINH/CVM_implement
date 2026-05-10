71fa787c1c3e3a83d6ed4e2ef1cb7984422489e8
NGOCLINH-NLINH <124993579+NGOCLINH-NLINH@users.noreply.github.com>
2026-05-10 22:59:18 +0700

    ehe

--- scripts/train_cvm.py
+++ scripts/train_cvm.py
@@ -331,7 +331,7 @@ def main(cfg):
                             # decay_factor = 1.0 / (1.0 + 0.15 * t)
                             # cur_lambda = cfg['replay_lambda'] * decay_factor
                             # cur_beta = cfg.get('beta', 0.0) * decay_factor
-                            loss += cfg['replay_lambda'] * (Lm_buf + cur_beta * Ld_buf)
+                            loss += cfg['replay_lambda'] * (Lm_buf + cfg['beta'] * Ld_buf)
 
                             # loss += cfg['replay_lambda'] * (Lm_buf + cfg['beta'] * Ld_buf)
 
