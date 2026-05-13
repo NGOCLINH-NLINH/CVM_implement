52fa02045def206710cbdda8092f437c5f0a768e
NGOCLINH-NLINH <124993579+NGOCLINH-NLINH@users.noreply.github.com>
2026-05-13 09:50:23 +0700

    semantic_distance_loss cu

--- utils/utils.py
+++ utils/utils.py
@@ -105,8 +105,7 @@ def semantic_distance_loss(emb, emb_prev, old_anchor_matrix):
     cos_prev = emb_prev @ old_anchor_matrix.t()
     d_t = 1.0 - cos_t
     d_prev = 1.0 - cos_prev
-    # return F.mse_loss(d_t, d_prev)
-    return F.mse_loss(d_t, d_prev, reduction='none').sum(dim=1).mean()
+    return F.mse_loss(d_t, d_prev)
 
 
 def make_cifar100_tasks(num_tasks, batch_size, augment=True):
