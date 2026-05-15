a852a02e295e98c3056549b115414551dbd3be50
NGOCLINH-NLINH <124993579+NGOCLINH-NLINH@users.noreply.github.com>
2026-05-15 21:42:47 +0700

    sua loss_k_negs

--- utils/utils.py
+++ utils/utils.py
@@ -171,9 +171,10 @@ def triplet_loss_k_negs(emb, pos_emb, neg_embs, margin=0.1):
 
     cos_neg = (emb.unsqueeze(1) * neg_embs).sum(dim=2)
     d_neg = 1.0 - cos_neg
-    loss = torch.clamp(d_pos.unsqueeze(1) - d_neg + margin, min=0.0)
-
-    return loss.mean()
+    loss_mat = torch.clamp(d_pos.unsqueeze(1) - d_neg + margin, min=0.0)
+    loss_per_sample = loss_mat.sum(dim=1)
+    return loss_per_sample.mean()
+    # return loss.mean()
 
 
 def triplet_loss_seen_negs(emb, pos_emb, labels, anchors_tensor, seen_indices, margin=0.1):
@@ -256,13 +257,15 @@ def adaptive_margin_triplet_loss_k_negs(emb, pos, neg_k, base_margin=0.1, reduct
     anchor_sim = (pos.unsqueeze(1) * neg_k).sum(dim=2)
 
     adaptive_margin = base_margin * (1.0 - anchor_sim).clamp(min=0.0)
-    loss = F.relu(sim_neg - sim_pos + adaptive_margin)
-    if reduction == "mean":
-        return loss.mean()
-    elif reduction == "sum":
-        return loss.sum()
-    else:
-        return loss
+    loss_mat = F.relu(sim_neg - sim_pos + adaptive_margin)
+    loss_per_sample = loss_mat.sum(dim=1)
+    return loss_per_sample.mean()
+    # if reduction == "mean":
+    #     return loss.mean()
+    # elif reduction == "sum":
+    #     return loss.sum()
+    # else:
+    #     return loss
 
 
 # def make_cifar100_tasks(num_tasks, batch_size, augment=True):
